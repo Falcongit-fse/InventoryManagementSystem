23ed0e23a5eade6a2990eb69094707ac232768c4
cognizantjava14 <cognizantjava14@cogjava128.iiht.tech>
2020-08-03 08:25:52 +0530

    old code

--- src/main/java/com/inventory/ProductApplication.java
+++ src/main/java/com/inventory/ProductApplication.java
@@ -2,7 +2,7 @@ package com.inventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 import springfox.documentation.builders.PathSelectors;
@@ -12,7 +12,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 @EnableSwagger2
 public class ProductApplication {
 
